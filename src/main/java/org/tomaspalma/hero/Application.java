6cee04e78ed8cf1ff55edb690739434e221c41c1
tomaspalma <tomaspalma@pop-os.localdomain>
2022-09-29 10:22:05 +0100

    Fixed error handling structure to main

--- src/main/java/org/tomaspalma/hero/Application.java
+++ src/main/java/org/tomaspalma/hero/Application.java
@@ -13,7 +13,11 @@ import java.io.IOException;
 public class Application {
     public static void main(String[] args) {
         Game game = new Game();
-        game.run();
+        try {
+            game.run();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- src/main/java/org/tomaspalma/hero/Game.java
+++ src/main/java/org/tomaspalma/hero/Game.java
@@ -2,6 +2,7 @@ package org.tomaspalma.hero;
 
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -27,21 +28,25 @@ public class Game {
         }
     }
     //Methods
-    private void draw() {
+    private void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')
-                [0]);
-        try {
-            screen.refresh();
-        } catch(IOException e) {
-            e.printStackTrace();
-        }
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
+        screen.refresh();
     }
 
-    public void run() {
-        this.draw();
+    public void run() throws IOException {
+        KeyStroke key;
+
+        draw();
+        key = screen.readInput(); processKey(key);
+    }
+
+    // Processar uma tecla lida pelo programada
+    private void processKey(KeyStroke key) {
+        System.out.println(key);
     }
 
     //Attributes
     private Screen screen;
+    private int x = 10, y = 10;
 }
