61adc9cb61421c1ce67c5d1ccd598ab39ac8db04
Tomás Palma <up202108880@edu.fe.up.pt>
2022-10-03 01:03:16 +0100

    Added monster class

--- src/main/java/org/tomaspalma/hero/Monster.java
+++ src/main/java/org/tomaspalma/hero/Monster.java
@@ -1,4 +1,25 @@
 package org.tomaspalma.hero;
 
-public class Monster {
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+
+import java.util.Random;
+
+public class Monster extends Element {
+    public Monster(int x, int y) {
+        super(x, y);
+    }
+
+    public Position move() {
+        return new Position(0, 0);
+    }
+
+    @Override
+    public void draw(TextGraphics graphics) {
+        graphics.setForegroundColor(TextColor.Factory.fromString("#006400"));
+        graphics.enableModifiers(SGR.BOLD);
+        graphics.putString(new TerminalPosition(currentPosition.getX(), currentPosition.getY()), "M");
+    }
 }
